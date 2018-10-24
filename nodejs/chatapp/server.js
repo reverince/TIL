30e31d381fc59b1014ec80c45e227cbfde92479e
Levdev <reverince@gmail.com>
2018-10-24 01:14:46 +0000

    Enhance nodejs chatapp with colored name

--- nodejs/chatapp/server.js
+++ nodejs/chatapp/server.js
@@ -5,6 +5,7 @@ var _ = require('lodash');
 
 var port = process.env.PORT || 3000;
 var names = {};
+var user_colors = {};
 var typers = new Set();
 
 const SYS = '[SYS]';
@@ -15,14 +16,46 @@ const USERS_NOTICE_WITH = '참가 중인 유저: ';
 const TYPERS_NOTICE_WITH = '입력 중인 유저: ';
 const CANNOT_FIND_USER_MESSAGE = '해당 유저를 찾을 수 없어요.';
 
+String.prototype.hashCode = function() {
+  var hash = 0, i, chr;
+  if (this.length === 0) return hash;
+  for (i = 0; i < this.length; i++) {
+    chr   = this.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + chr;
+    hash |= 0;  // Convert to 32bit integer
+  }
+  return hash;
+};
+
+Number.prototype.pad = function(len) {
+  var ret = '' + this;
+  if (ret.length >= len) {
+    return ret.substring(0, len);
+  }
+  while (ret.length < len) {
+    ret = '0' + ret;
+  }
+  return ret;
+}
+
+String.prototype.generateColor = function() {
+  var hash = this.hashCode();
+  var code = (hash & 0xFFFFFF).pad(6);
+  var ret = '#' + code;
+  console.log(ret);
+  return ret;
+}
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+
 
 io.on('connection', (socket) => {
   
   socket.on('join', (name) => {
     names[socket.id] = name;
+    user_colors[socket.id] = name.generateColor();
     console.log('유저 참가: ' + name);
     let data = {"name": SYS, "message": CONNECTED_MESSAGE_WITH + name};
     io.emit('chat message', data);
@@ -49,8 +82,9 @@ io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
     if (socket.id in names) {
       var author = names[socket.id];
+      var color = user_colors[socket.id];
       console.log(author + ': ' + msg);
-      let data = {"name": author, "message": msg};
+      let data = {"name": author, "message": msg, "color": color};
       io.emit('chat message', data);
     } else {
       console.log('미참가 유저의 채팅 시도: ' + socket.id);
@@ -88,10 +122,6 @@ io.on('connection', (socket) => {
       let data = {"name": SYS, "message": CANNOT_FIND_USER_MESSAGE};
       socket.emit('chat message', data);
     }
-  });
-  
-  socket.on('new room', (room) => {
-    io.to(room).emit('chat message', data);
   });
   
   function updateUsers() {
